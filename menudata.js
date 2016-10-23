865c60c61f2b13604b3786e443fc72d7b338b5f5
Edouard DUPIN <yui.heero@gmail.com>
2016-10-23 23:20:07 +0200

    [DOC] correct the doxygen display

---
+++ menudata.js
@@ -0,0 +1,24 @@
+var menudata={children:[
+{text:'Main Page',url:'index.html'},
+{text:'Related Pages',url:'pages.html'},
+{text:'Namespaces',url:'namespaces.html',children:[
+{text:'Namespace List',url:'namespaces.html'},
+{text:'Namespace Members',url:'namespacemembers.html',children:[
+{text:'All',url:'namespacemembers.html',children:[
+{text:'c',url:'namespacemembers.html#index_c'},
+{text:'d',url:'namespacemembers.html#index_d'},
+{text:'g',url:'namespacemembers.html#index_g'},
+{text:'i',url:'namespacemembers.html#index_i'},
+{text:'l',url:'namespacemembers.html#index_l'},
+{text:'r',url:'namespacemembers.html#index_r'},
+{text:'s',url:'namespacemembers.html#index_s'},
+{text:'u',url:'namespacemembers.html#index_u'}]},
+{text:'Functions',url:'namespacemembers_func.html'},
+{text:'Typedefs',url:'namespacemembers_type.html'},
+{text:'Enumerations',url:'namespacemembers_enum.html'},
+{text:'Enumerator',url:'namespacemembers_eval.html'}]}]},
+{text:'Files',url:'files.html',children:[
+{text:'File List',url:'files.html'},
+{text:'File Members',url:'globals.html',children:[
+{text:'All',url:'globals.html'},
+{text:'Macros',url:'globals_defs.html'}]}]}]}
